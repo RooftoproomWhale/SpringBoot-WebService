505e7e6a9e007669eda7c1ce7d0187d1a2659293
RooftoproomWhale <gh8238@naver.com>
2021-05-03 21:52:53 +0900

    0503 mustache, board CRUD

--- src/main/java/com/geonung/spring/boot/service/posts/PostsService.java
+++ src/main/java/com/geonung/spring/boot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.geonung.spring.boot.service.posts;
 
 import com.geonung.spring.boot.domain.posts.Posts;
 import com.geonung.spring.boot.domain.posts.PostsRepository;
+import com.geonung.spring.boot.web.dto.PostsListResponseDto;
 import com.geonung.spring.boot.web.dto.PostsResponseDto;
 import com.geonung.spring.boot.web.dto.PostsSaveRequestDto;
 import com.geonung.spring.boot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,22 @@ public class PostsService {
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc()
+    {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id)
+    {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
